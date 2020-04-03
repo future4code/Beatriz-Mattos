a97ae85f26731dc931be6b7adf17ec328934a530
Beatriz Mattos <venganzakills@gmail.com>
2020-04-03 16:52:17 -0300

    entrega incompleta do projeto

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,38 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+iniciarJogo();
+
+let pontosUsuario = 0;
+let pontosComputador = 0;
+let cartasUsuario = [];
+let cartasComputador = [];
+
+function iniciarJogo() {
+   console.log("Bem-vindo ao jogo de Blackjack!");
+   if (confirm("Quer iniciar uma nova rodada?")) {
+      while (usuario <= 21 || computador <= 21) {
+         let carta = comprarCarta();
+         cartasUsuario.push(carta.texto);
+         pontosUsuario.push(carta.valor);
+         let carta = comprarCarta();
+         cartasComputador.push(carta.texto);
+         pontosComputador.push(carta.valor);
+      }
+       {
+         
+
+      }
+   } else {
+      console.log("O jogo acabou.");
+   }
+}
