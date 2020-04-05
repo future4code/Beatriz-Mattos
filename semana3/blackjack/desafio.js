0a06367521536dbae0240acdd223ed59c5250c65
Beatriz Mattos <venganzakills@gmail.com>
2020-04-05 02:21:50 -0300

    iniciando desafio.js

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,52 @@
  * 
  */
 
+let maoUsuario = [];
+let maoComputador = [];
+let somaUsuario = 0;
+let somaComputador = 0;
+
+desafioJogo();
+
+function desafioJogo() {
+   if (confirm("Quer iniciar uma nova rodada?")) {
+      
+      for (let i=0; i<2; i++) {
+         const cartaUsuario = comprarCarta();
+         const cartaComputador = comprarCarta();
+
+         somaUsuario += cartaUsuario.valor;
+         somaComputador += cartaComputador.valor;
+                  
+         maoUsuario.push(cartaUsuario);
+         maoComputador.push(cartaComputador);
+      }
+
+         alert("Suas cartas são " + maoUsuario[0].texto + " e " + maoUsuario[1].texto + ". A carta revelada do computador é " +maoComputador[0].texto+ ". Deseja comprar mais uma carta?");
+         
+         if (alert === "Ok"){
+            const cartaUsuario = comprarCarta();
+            const cartaComputador = comprarCarta();
+
+            somaUsuario += cartaUsuario.valor;
+            somaComputador += cartaComputador.valor;
+                     
+            maoUsuario.push(cartaUsuario);
+            maoComputador.push(cartaComputador);
+
+            alert("Suas cartas são " + maoUsuario[2].texto + " e " + maoUsuario[3].texto + ". As cartas do computador são " + maoComputador[2].texto+ " e " + maoComputador[3] + ". A pontuação do computador é " + somaComputador);
+         }
+
+   } else {
+      console.log("O jogo acabou.")
+   }
+}   
+
+
+
+    // while (pontosUsuario <= 21 || pontosComputador <= 21) {
+         //    cartasUsuario.push(carta.texto);
+         //    pontosUsuario += carta.valor;
+         //    cartasComputador.push(carta.texto);
+         //    pontosComputador += carta.valor;
+         // }
