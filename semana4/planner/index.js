44de58988acd3018ab39fd9ea2cf510316726848
Beatriz Mattos <venganzakills@gmail.com>
2020-04-07 16:44:29 -0300

    entrega parcial do planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,27 @@
+function criarTarefa() {
+    const tarefa = document.getElementById("novaTarefa").value;
+    const semana = document.getElementById("diaSemana").value;
+    
+}
+
+function adicionarItem() {
+
+}
+
+
+// function adicionarItem() {
+//     const input = document.querySelector("input")
+    
+//     const novoItem = input.value
+    
+//     if(novoItem === ""){
+//         alert("Insira um item!!!")
+//     } else {
+//         const listaDeCompras = document.querySelector("ul")
+
+//         listaDeCompras.innerHTML += "<li>" + novoItem + "</li>"
+    
+//         input.value = ""
+//     }
+// }
+
