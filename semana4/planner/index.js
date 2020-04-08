4f58e727e9288f010410e8772002bbd54c13bd52
Beatriz Mattos <venganzakills@gmail.com>
2020-04-07 23:19:32 -0300

    atualizando requisitos do planner

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,27 +1,42 @@
 function criarTarefa() {
-    const tarefa = document.getElementById("novaTarefa").value;
-    const semana = document.getElementById("diaSemana").value;
-    
+    const meuInput = document.getElementById("meu-input");
+    const diaSemana = document.getElementById("dia-semana");
+
+    if (meuInput.value === "") {
+        alert("Ops, tarefa vazia :(");
+    } else {
+        switch (diaSemana.value) {
+                case "domingo":
+                    document.getElementById("tarefa-domingo").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+
+                case "segunda":
+                    document.getElementById("tarefa-segunda").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+
+                case "terca":
+                    document.getElementById("tarefa-terca").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+                
+                case "quarta":
+                    document.getElementById("tarefa-quarta").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+                
+                case "quinta":
+                    document.getElementById("tarefa-quinta").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+                
+                case "sexta":
+                    document.getElementById("tarefa-sexta").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+                
+                case "sabado":
+                    document.getElementById("tarefa-sabado").innerHTML += "<li>" + meuInput.value + "</li>";
+                    break;
+
+                default:
+                    console.log("")
+        }
+    }
+
 }
-
-function adicionarItem() {
-
-}
-
-
-// function adicionarItem() {
-//     const input = document.querySelector("input")
-    
-//     const novoItem = input.value
-    
-//     if(novoItem === ""){
-//         alert("Insira um item!!!")
-//     } else {
-//         const listaDeCompras = document.querySelector("ul")
-
-//         listaDeCompras.innerHTML += "<li>" + novoItem + "</li>"
-    
-//         input.value = ""
-//     }
-// }
-
