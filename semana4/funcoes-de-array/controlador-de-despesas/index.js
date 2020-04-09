f644a85b6e8d89e9baffc19816c7b61843d2740d
Beatriz Mattos <venganzakills@gmail.com>
2020-04-09 19:44:24 -0300

    quase lá

--- semana4/funcoes-de-array/controlador-de-despesas/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -1,7 +1,45 @@
+const arrayDespesa = [];
+const arrayFiltro = [];
+
+
+const objetoDespesa = {
+    valor: document.getElementById("valor-despesa").value,
+    tipo: document.getElementById("select-controlador-despesas").value,
+    descricao: document.getElementById("descricao-despesa").value;
+}
+
+
+if (valor === ""){
+    alert("Esqueceu de digitar o valor!")
+} else if (tipo === "") {
+    alert("Esqueceu de selecionar o tipo!")
+} else if (descricao === "") {
+    alert("Esqueceu de digitar a descrição!")
+}
+
+
+
 function cadastrarDespesa(){
     const valorDespesa = document.getElementById("valor-despesa").value;
     const tipoDespesa = document.getElementById("select-controlador-despesas").value;
     const descricaoDespesa = document.getElementById("descricao-despesa").value;
     const areaMostraDespesa = document.getElementById("area-despesas-detalhadas").value;
-    areaMostraDespesa.innerHTML += 
+    areaMostraDespesa.innerHTML += "<p>Despesa:</p> <p>Valor: " + valorDespesa + "</p>" + "<p>Tipo: " + tipoDespesa + "</p>" + "<p>Descrição: " + descricaoDespesa + "</p>"
+
+    arrayDespesa.push(objetoDespesa);
+
+    objetoDespesa.valor = document.getElementById("valor-despesa").value;
+    objetoDespesa.tipo = document.getElementById("select-contolador-despesas").value;
+    objetoDespesa.descricao = document.getElementById("descricao-despesa").value;
+    return filtrarDespesas();
+}
+
+
+function filtrarDespesas() {
+
+}
+
+
+function limparCampos() {
+ 
 }
