d6a4fc76621d732000cf4dbaa1e395e3fa2891d0
Beatriz Mattos <venganzakills@gmail.com>
2020-04-09 17:03:16 -0300

    entrega parcial do controlador de despesas

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,7 @@
+function cadastrarDespesa(){
+    const valorDespesa = document.getElementById("valor-despesa").value;
+    const tipoDespesa = document.getElementById("select-controlador-despesas").value;
+    const descricaoDespesa = document.getElementById("descricao-despesa").value;
+    const areaMostraDespesa = document.getElementById("area-despesas-detalhadas").value;
+    areaMostraDespesa.innerHTML += 
+}
