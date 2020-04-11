32b9d7ab62b26137a951256bf970d79174a77c74
Beatriz Mattos <venganzakills@gmail.com>
2020-04-11 00:48:45 -0300

    Exercício 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,12 @@
+/* Exercícios de leitura de código - Exercício 1
+
+O trecho de código exibe uma função capaz de converter um valor em dólar para reais, considerando o valor inserido pelo usuário.
+A funcão é iniciada com o parâmetro valorEmDolar.
+Dentro do escopo da função, é declarada uma variável que recebe os dados que o usuário inseriu no prompt, convertidos com a função Number().
+É retornado um valor que concatena a string "R$" com o resultado do parâmetro valorEmDolar multiplicado com a cotação inserida pelo usuário.
+Fora do escopo, é declarada uma variável que "invoca" a função acima  recebendo o valor retornado da função e passa 100 como parâmetro.
+Em seguida, é impresso o valor da última variável no console.
+
+Se eu digitasse o valor 10 no prompt, seria imprimido R$1000 no console, pois 10 * 100 = 1000;
+
+*/
