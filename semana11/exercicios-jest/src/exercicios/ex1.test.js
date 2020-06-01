95c4b1a6f66499c7bae0c79b6bb76467360f8d63
Beatriz Mattos <venganzakills@gmail.com>
2020-06-01 16:31:29 -0300

    Exercícios - testes unitários

---
+++ semana11/exercicios-jest/src/exercicios/ex1.test.js
@@ -0,0 +1,21 @@
+import { checaBissexto } from "./ex1";
+
+describe("Checa bissexto", () => {
+  test("retorna true pra 1600", () => {
+    const resultado = checaBissexto(1600);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna true pra 2000", () => {
+    const resultado = checaBissexto(2000);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna true pra 1996", () => {});
+
+  test("retorna true pra 2008", () => {});
+
+  test("retorna false pra 2007", () => {});
+});
---
+++ semana11/exercicios-jest/src/exercicios/ex2.test.js
@@ -0,0 +1,8 @@
+import { checaPalindromo } from "./ex2";
+
+describe("Checa Palíndromo", () => {
+  it("retorna true para 'mirim'", () => {
+    const ehPalindromo = checaPalindromo("mirim");
+    expect(ehPalindromo).toEqual(true);
+  });
+});
