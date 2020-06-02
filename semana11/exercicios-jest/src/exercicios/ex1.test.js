b07d7cc379034a1a8b713769137ed9ad4f1798f8
Beatriz Mattos <venganzakills@gmail.com>
2020-06-01 22:23:27 -0300

    ex1 concluído

--- semana11/exercicios-jest/src/exercicios/ex1.test.js
+++ semana11/exercicios-jest/src/exercicios/ex1.test.js
@@ -13,9 +13,27 @@ describe("Checa bissexto", () => {
     expect(resultado).toEqual(true);
   });
 
-  test("retorna true pra 1996", () => {});
+  test("retorna true pra 1996", () => {
+    const resultado = checaBissexto(1996);
 
-  test("retorna true pra 2008", () => {});
+    expect(resultado).toEqual(true);
+  });
 
-  test("retorna false pra 2007", () => {});
+  test("retorna true pra 2008", () => {
+    const resultado = checaBissexto(2008);
+
+    expect(resultado).toEqual(true);
+  });
+
+  test("retorna false pra 2007", () => {
+    const resultado = checaBissexto(2007);
+
+    expect(resultado).toEqual(false);
+  });
+
+  test("retorna true para 2020", () => {
+    const resultado = checaBissexto(2020);
+
+    expect(resultado).toEqual(true);
+  });
 });
