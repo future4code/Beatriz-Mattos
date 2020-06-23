d61de8ef8e2bf9244d331f4832587e8d22aa4031
Beatriz Mattos <venganzakills@gmail.com>
2020-06-23 20:18:05 -0300

    concluindo exercicio 3

--- semana14/exercicios-node-package/src/exercicio-3.ts
+++ semana14/exercicios-node-package/src/exercicio-3.ts
@@ -3,13 +3,13 @@ import * as fs from 'fs';
 
 //Crie uma aplicação Node que receba uma string representando o nome do arquivo da lista de tarefas e uma string representando uma nova tarefa, em seguida o programa deve adicionar a nova tarefa em um arquivo que tenha o nome da lista de tarefas. Para isso, crie um arquivo chamado tarefas.txt
 
-const arquivoTarefas: string = 'tarefas.txt';
-const dadoRecebido: string = process.argv[2];
+const arquivo: string = 'tarefas.txt';
+const dado: string = process.argv[2];
 
 try {
-    fs.appendFileSync('tarefas.txt', 'e estudar node.js');
+    fs.appendFileSync(arquivo, dado);
     console.log('Tarefa adicionada com sucesso!')
 }
-catch {
-    console.log('Não foi possível enviar sua tarefa para o arquivo.')
+catch (err) {
+    console.log('Não foi possível enviar sua tarefa para o arquivo. Erro: ', err);
 }
