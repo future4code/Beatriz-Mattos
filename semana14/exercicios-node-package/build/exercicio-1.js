f912d927d3a323bc68c70fe204b857f9c5784761
Beatriz Mattos <venganzakills@gmail.com>
2020-06-23 19:11:34 -0300

    exercício 1

--- semana14/exercicios-node-package/build/exercicio-1.js
+++ semana14/exercicios-node-package/build/exercicio-1.js
@@ -1,4 +1,9 @@
 "use strict";
-console.log(process.argv[2]);
-console.log('olá');
+function retornaDados(nome, idade) {
+    return nome + idade;
+}
+const nome = process.argv[2];
+const idade = Number(process.argv[3]);
+const mensagem2 = `Olá ${nome}! Você tem ${idade} anos. Em sete anos, você terá ${idade + 7}.`;
+console.log(mensagem2);
 //# sourceMappingURL=exercicio-1.js.map
--- semana14/exercicios-node-package/src/exercicio-1.ts
+++ semana14/exercicios-node-package/src/exercicio-1.ts
@@ -4,19 +4,20 @@
 //resposta: Acessamos argumentos passados aos nossos scripts através do process.argv;
 
 //b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura: "Olá, (Nome)! Você tem (sua idade) anos."
-// function recebeDados(): void => {
-//     const nome: string = process.argv[2];
-//     const idade: number = Number(process.argv[3]);
-    
-//     console.log(`Olá, ${nome}! Você tem ${idade} anos.`);
-//     console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade+7}`);
-// };
 
-// recebeDados();
+function retornaDados(nome: string, idade: number): string {
+    return nome + idade
+}
 
-// function somar(a: number, b: number): number {
-//     return a + b
-// }
+const nome: string = process.argv[2];
+const idade: number = Number(process.argv[3]);
 
-console.log(process.argv[2])
-console.log('olá')
+//const mensagem: string = `Olá, ${nome}! Você tem ${idade} anos.`
+
+//console.log(mensagem)
+
+//c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
+
+const mensagem2: string = `Olá, ${nome}! Você tem ${idade} anos. Em sete anos, você terá ${idade + 7}.`
+
+console.log(mensagem2)
