1f1fe5e3ce6b5b4b6ce7d6c241edae6b93ce0758
Beatriz Mattos <venganzakills@gmail.com>
2020-06-29 23:56:56 -0300

    iniciando refatoração do Labebank

---
+++ semana15/exercicio-classes/src/Transaction.ts
@@ -0,0 +1,11 @@
+export class Transaction {
+    private date: string;
+    private value: number;
+    private description: string;
+    
+    constructor(date: string, value: number, description: string) {
+      this.date = date;
+      this.value = value;
+      this.description = description
+    }
+  }
