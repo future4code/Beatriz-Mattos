81df26b3d2c6c6bc638d6374bcd7445d38331c6e
Beatriz Mattos <venganzakills@gmail.com>
2020-08-24 19:14:19 -0300

    exercicio 2

--- semana23/resolvendo-problemas/src/index.ts
+++ semana23/resolvendo-problemas/src/index.ts
@@ -19,8 +19,41 @@ const isOneEdit = (original: string, comparision: string): boolean => {
     );
 };
 
-console.log(isOneEdit("banana", "banan"));
-console.log(isOneEdit("banana", "bananak"));
-console.log(isOneEdit("banana", "panana"));
-console.log(isOneEdit("banana", "bananaaa"));
+// console.log(isOneEdit("banana", "banan"));
+// console.log(isOneEdit("banana", "bananak"));
+// console.log(isOneEdit("banana", "panana"));
+// console.log(isOneEdit("banana", "bananaaa"));
 
+
+/* Exercício 2 */
+
+const stringCompression = (input: string): string => {
+    const substrings: string[] = [];
+    let lastChar = input[0];
+    let charCount = 0;
+
+    for (const char of input) {
+        if (char !== lastChar) {
+            substrings.push(lastChar + charCount);
+            lastChar = char;
+            charCount = 0;
+        }
+        
+        charCount++;
+    };
+
+    substrings.push(lastChar + charCount);
+    let result = "";
+    for (const key of substrings) {
+        result += key
+    };
+
+    /*Caso o resultado final tenha tamanho maior do que a string inicial, seu programa deve retornar a string inicial */
+    return result.length > input.length ? input : result;
+};
+
+console.log(stringCompression("aabb"));
+console.log(stringCompression("aabcccccaaa"));
+console.log(stringCompression("accurate"));
+console.log(stringCompression("escola"));
+console.log(stringCompression("accuraaaaaaaaaate"));
