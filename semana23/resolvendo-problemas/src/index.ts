5d4a3c20a660f3316f0272f1f1e950943375b68e
Beatriz Mattos <venganzakills@gmail.com>
2020-08-24 22:12:51 -0300

    exercicio 3 até 4

--- semana23/resolvendo-problemas/src/index.ts
+++ semana23/resolvendo-problemas/src/index.ts
@@ -15,7 +15,7 @@ const isOneEdit = (original: string, comparision: string): boolean => {
 
     return (
         matchingCharQuantity <= (original.length + 1) &&
-        matchingCharQuantity >= (original.length -1)
+        matchingCharQuantity >= (original.length - 1)
     );
 };
 
@@ -38,7 +38,7 @@ const stringCompression = (input: string): string => {
             lastChar = char;
             charCount = 0;
         }
-        
+
         charCount++;
     };
 
@@ -52,8 +52,108 @@ const stringCompression = (input: string): string => {
     return result.length > input.length ? input : result;
 };
 
-console.log(stringCompression("aabb"));
-console.log(stringCompression("aabcccccaaa"));
-console.log(stringCompression("accurate"));
-console.log(stringCompression("escola"));
-console.log(stringCompression("accuraaaaaaaaaate"));
+// console.log(stringCompression("aabb"));
+// console.log(stringCompression("aabcccccaaa"));
+// console.log(stringCompression("accurate"));
+// console.log(stringCompression("escola"));
+// console.log(stringCompression("accuraaaaaaaaaate"));
+
+
+/* Exercício 3 */
+
+/* a) A matriz é um array dentro da posição de outro array, como uma tabela que abriga um array em cada linha. Logo, para representarmos uma matriz em TypeScript, podemos representar como um array de arrays de número. */
+[[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]
+
+/* b) Escreva uma função que receba uma Matriz, o índice da linha, o índice da coluna e um valor. */
+const replaceMatrixValue = (matrix: number[][], rowIndex: number, columnIndex: number, value: number): void => {
+    /* Essa função deve alterar o elemento da linha e coluna em questão pelo valor passado. Caso não tenha nenhum valor nos índices passados, retorne um erro. */
+    if (matrix[rowIndex] === undefined && matrix[rowIndex][columnIndex] === undefined) {
+        throw new Error("Out of matrix range")
+    };
+
+    matrix[rowIndex][columnIndex] = value;
+};
+
+
+/* Exercício 4 */
+
+/* a. Implemente uma função que receba uma matriz e imprima, no console, todos os seus elementos */
+const printMatrix = (matrix: number[][]): void => {
+    for (const row of matrix) {
+        for (const element of row) {
+            console.log(element)
+        }
+    };
+};
+
+
+/* b. Implemente uma função que receba duas matrizes e devolva a soma delas */
+const sumMatrix = (m1: number[][], m2: number[][]): number[][] => {
+    const newMatrix: number[][] = [];
+
+    if (m1.length !== m2.length || m1[0].length !== m2[0].length) {
+        throw new Error("Matrix dimensions must be equal")
+    };
+
+    const rowSize = m1[0].length;
+    const columnSize = m2[0].length;
+
+    for (let i = 0; i < rowSize; i++) {
+        let newMatrixRow = [];
+        for (let j = 0; j < columnSize; j++) {
+            newMatrixRow.push(m1[i][j] + m2[i][j]);
+        }
+        newMatrix.push(newMatrixRow);
+    }
+
+    return newMatrix;
+};
+
+
+/* c. Implemente uma função que receba uma matriz e devolva a sua matriz transposta */
+
+const transposeMatrix = (m: number[][]): number[][] => {
+    const transposed: number[][] = [];
+    if (!m.length) {
+        throw new Error("Matriz inválida");
+    }
+    for (let j = 0; j < m[0].length; j++) {
+        let row = [];
+        for (let i = 0; i < m.length; i++) {
+            row.push(m[i][j]);
+        }
+        transposed.push(row);
+    }
+    return transposed;
+};
+
+/* d. Implemente uma função que receba duas matrizes e devolva a multiplicação delas */
+
+const multiplyMatrix = (m1: number[][], m2: number[][]): number[][] => {
+    const result: number[][] = [];
+
+    if (m1[0].length !== m2[0].length) {
+        throw new Error("Invalid matrices")
+    };
+
+    for (let i = 0; i < m1.length; i++) {
+        let rowResult: number[] = [];
+
+        for (let j = 0; j < m2[0].length; j++) {
+            let sumResult = 0;
+
+            for (let k = 0; k < m1[0].length; k++) {
+                sumResult += m1[i][k] * m2[k][j];
+            }
+
+            rowResult.push(sumResult);
+        }
+
+        result.push(rowResult);
+    }
+
+    return result;
+};
+
