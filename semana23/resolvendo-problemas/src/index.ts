f49334ec63596802897e751d0def5296902ac247
Beatriz Mattos <venganzakills@gmail.com>
2020-08-24 22:21:59 -0300

    exercicio 5

--- semana23/resolvendo-problemas/src/index.ts
+++ semana23/resolvendo-problemas/src/index.ts
@@ -157,3 +157,32 @@ const multiplyMatrix = (m1: number[][], m2: number[][]): number[][] => {
     return result;
 };
 
+
+/* Exercício 5 */
+/* [Amazon] Considere que você tenha uma matriz de números cujas linhas e colunas estejam ordenadas da menor para a maior. Determine a quantidade de números negativos que há dentro da matriz. */
+
+/* A seguinte matriz está ordenada (em relação às linhas e as colunas). A quantidade de números negativos é 4 */
+[
+    [-3, -2, -1, 1],
+    [-2, 2, 3, 4],
+    [4, 5, 7, 8]
+]
+
+const findNegativeNumbersInMatrix = (matrix: number[][]): number => {
+    let count = 0;
+    let row = 0;
+    let column = matrix[0].length -1;
+
+    while (row < matrix.length && column >= 0) {
+
+        if (matrix[row][column] < 0) {
+            count += column + 1;
+            row++;
+
+        } else {
+            column--;
+        }
+    }
+
+    return count;
+};
