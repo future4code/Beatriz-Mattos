c4f632a09a7e35108530b9b9ef254cd35652458e
Beatriz Mattos <venganzakills@gmail.com>
2020-08-26 16:36:29 -0300

    exercício 1-b

--- semana23/recursividade/src/index.ts
+++ semana23/recursividade/src/index.ts
@@ -7,4 +7,15 @@ const printNumbers = (n: number) => {
     }
 };
 
-console.log(printNumbers(7));
+//printNumbers(7);
+
+
+/* b. Receba um número e imprima todos os inteiros desse número até 0 em ordem decrescente */
+const printDecrescentNumbers = (n: number) => {
+    if(n >= 0) {
+        console.log(n);
+        printDecrescentNumbers(n - 1);
+    }
+};
+
+printDecrescentNumbers(7);
